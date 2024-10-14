60eb3359c4b06a6911ce3d886367287890340399
multipletwigs <zachkhong.workplace@gmail.com>
2024-10-14 11:51:09 +0800

    feat: added OG tags!

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,38 @@ export default function RootLayout({
   const isLoginModalOpen = useAuthStore((state) => state.isLoginModalOpen);
   return (
     <html lang="en">
-      <head></head>
+      <head>
+        <title>Pinata OG!</title>
+        <meta
+          name="description"
+          content="Generate beautiful OG Images with Pinata File API!"
+        />
+
+        <meta property="og:url" content="https://pinata.nightly.ink" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Pinata OG!" />
+        <meta
+          property="og:description"
+          content="Generate beautiful OG Images with Pinata File API!"
+        />
+        <meta
+          property="og:image"
+          content="https://pinata.nightly.ink/api/get-og?cid=bafybeif4gfptwk3ef6oy5lyh62o2itcln6e72yrl57zzsrakslas4mgiwy"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="https://pinata.nightly.ink" />
+        <meta property="twitter:url" content="https://pinata.nightly.ink" />
+        <meta name="twitter:title" content="Pinata OG!" />
+        <meta
+          name="twitter:description"
+          content="Generate beautiful OG Images with Pinata File API!"
+        />
+        <meta
+          name="twitter:image"
+          content="https://pinata.nightly.ink/api/get-og?cid=bafybeif4gfptwk3ef6oy5lyh62o2itcln6e72yrl57zzsrakslas4mgiwy"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
